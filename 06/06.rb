0a627a5bcc5fa3ad090e668503998c799ebd53ec
Matt Garden <matt.garden@hey.com>
2022-12-06 08:08:04 -0500

    Day 6 bonus solved

--- 06/06.rb
+++ 06/06.rb
@@ -2,8 +2,11 @@ def are_all_unique(chars)
   chars.length == chars.uniq.length
 end
 
-def find_marker_in(input)
-  i = 3
+MARKER_LENGTH = 4
+MESSAGE_LENGTH = 14
+
+def find_signal_in(input, signal_length)
+  i = signal_length - 1
   candidate = input[0..i]
   while i < input.length && !are_all_unique(candidate) do
     candidate.shift
@@ -14,5 +17,5 @@ def find_marker_in(input)
 end
 
 ARGF.each_line do |line|
-  find_marker_in line.split('')
+  find_signal_in(line.split(''), MESSAGE_LENGTH)
 end
