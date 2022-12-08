1a4246ade2610530874c26ffa54d69bf37345369
Matt Garden <matt.garden@hey.com>
2022-12-07 23:33:32 -0500

    Day 7 bonus

--- 07/07.rb
+++ 07/07.rb
@@ -16,36 +16,36 @@ class Directory
   def initialize(name, parent)
     @name = name
     @parent = parent
-    @children = {}
-    @files = {}
+    @children = []
+    @files = []
   end
 
   def size
-    @children.values.map(&:size).sum + @files.values.map(&:size).sum
+    @children.map(&:size).sum + @files.map(&:size).sum
   end
 end
 
-pwd = []
 root = Directory.new '/', nil
-all_dirs = []
+pwd = [root]
+all_dirs = [root]
 
 ARGF.each_line do |line|
-  p line
-
   if /cd ([[:alpha:]])+/ =~ line
     dir_name = $~.captures[0]
     dir = Directory.new(dir_name, pwd.last)
-    pwd.last.children[dir_name] = dir
+    pwd.last.children.push dir
     pwd.push dir
     all_dirs.push dir
-  elsif /cd \// =~ line
-    pwd = [root]
   elsif /cd \.\./ =~ line
     pwd.pop
   elsif /(\d+) ([[:alpha:]]+\.?[[:alpha:]]*)/ =~ line
     dir = pwd.last
-    dir.files[$~.captures[1]] = File.new($~.captures[1], Integer($~.captures[0]))
+    dir.files.push File.new($~.captures[1], Integer($~.captures[0]))
   end
 end
 
 p all_dirs.select { |d| d.size < 100000 }.map(&:size).sum
+
+unused = 70000000 - root.size
+needed = 30000000 - unused
+p all_dirs.sort_by { |d| d.size }.find { |d| d.size > needed }.size
